0b9b5123ca239bf32d0b43b9f5c5dff7d60468a2
Steven Cardini <steven@cardini.ch>
2017-11-20 18:31:15 +0100

    adds basic project structure

---
+++ src/main/java/ch/bfh/bti7535/w2017/midori/movieclassifier/App.java
@@ -0,0 +1,13 @@
+package ch.bfh.bti7535.w2017.midori.movieclassifier;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
