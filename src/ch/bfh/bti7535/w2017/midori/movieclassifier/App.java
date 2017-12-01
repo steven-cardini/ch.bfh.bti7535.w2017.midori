9234cf6e07fac409f6b4f4486b5981dd436779be
Thomas Buchegger <bucheggert@gmail.com>
2017-12-01 11:17:17 +0100

    arff importer added

--- src/ch/bfh/bti7535/w2017/midori/movieclassifier/App.java
+++ src/ch/bfh/bti7535/w2017/midori/movieclassifier/App.java
@@ -11,7 +11,11 @@ public class App
     public static void main( String[] args ) throws IOException
     {
         // https://weka.wikispaces.com/Programmatic+Use
-        InstanceLoader loader = new InstanceLoader();
+        //InstanceLoader loader = new InstanceLoader();
+    	
+    	ArffImporter imp = new ArffImporter();
+    	
+    	imp.imortArff("C:\\Users\\buche\\Documents\\Git\\ch.bfh.bti7535.w2017.midori\\data\\test.arff");
         
     }
 }
