1374280dd289c7958dd6c9a74beb3808e4001361
Thomas Buchegger <bucheggert@gmail.com>
2017-12-11 18:31:28 +0100

    bereinigt

--- src/ch/bfh/bti7535/w2017/midori/movieclassifier/converter/ArffGenerator.java
+++ src/ch/bfh/bti7535/w2017/midori/movieclassifier/converter/ArffGenerator.java
@@ -1,164 +1,3 @@
-<<<<<<< HEAD
-package ch.bfh.bti7535.w2017.midori.movieclassifier.converter;
-
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.regex.Pattern;
-
-import com.opencsv.CSVParser;
-import com.opencsv.CSVParserBuilder;
-import com.opencsv.CSVReader;
-import com.opencsv.CSVReaderBuilder;
-import com.opencsv.CSVWriter;
-
-import weka.core.Instances;
-import weka.core.converters.CSVLoader;
-
-public class ArffGenerator {
-
-  public enum Label {
-    POSITIVE,
-    NEGATIVE
-  }
-
-  private final static File lexiconInputFile = new File ("data" + File.separator + "general_inquirer_lexicon" + File.separator + "inquirerbasic.csv");
-  private static Map<String, WordConnotation> connotationLexicon = new HashMap<>();
-
-  private final static String trainingDataFolderPrefix = "data" + File.separator + "txt_sentoken" + File.separator;
-  private final static File negativeInstancesFolder = new File(trainingDataFolderPrefix + "neg");
-  private final static File positiveInstancesFolder = new File(trainingDataFolderPrefix + "pos");
-  
-  private final static File csvOutputFile = new File("data" + File.separator + "movie-reviews.csv");
-  private final static File arffOutputFile = new File("data" + File.separator + "movie-reviews.arff");
-
-  private final static List<String> relevantCols = Arrays.asList("Entry", "Positiv", "Negativ", "Hostile", "Strong", "Power", "Weak");
-  private final static Pattern wordDelimiterPattern = Pattern.compile("[^A-Za-z]");  
-  
-  
-  public static void main(String args[]) throws IOException {
-    
-    loadLexicon();
-    CSVWriter csvWriter = new CSVWriter(new FileWriter(csvOutputFile));
-    // write attribute titles to CSV file
-    csvWriter.writeNext(InstanceFeatures.titles());
-    
-    // iterate through all positively labeled files and add its features to the CSV file
-    File[] files = positiveInstancesFolder.listFiles();
-    for (File file : files) {
-      InstanceFeatures features = loadCommentFile(file, Label.POSITIVE);
-      csvWriter.writeNext(features.toStringArray());
-    }
-    
-    // iterate through all negatively labeled files and add its features to the CSV file
-    files = negativeInstancesFolder.listFiles();
-    for (File file : files) {
-      InstanceFeatures features = loadCommentFile(file, Label.NEGATIVE);
-      csvWriter.writeNext(features.toStringArray());
-    }
-    
-    csvWriter.close();
-    
-    // convert CSV file to ARFF file
-    CSVLoader csvLoader = new CSVLoader();
-    csvLoader.setSource(csvOutputFile);
-    Instances data = csvLoader.getDataSet();
-    data.setClassIndex(data.numAttributes()-1);
-        
-    BufferedWriter writer = new BufferedWriter(new FileWriter(arffOutputFile));
-    writer.write(data.toString());
-    writer.flush();
-    writer.close();
-    
-  }
-  
-  private static InstanceFeatures loadCommentFile(File file, Label label) throws FileNotFoundException {
-    
-    InstanceFeatures features = new InstanceFeatures(label);
-    Scanner in = new Scanner(file);
-    in.useDelimiter(wordDelimiterPattern);
-    while(in.hasNext()) {
-      String word = in.next()
-          .toLowerCase();
-      if(word.length() < 3 || !connotationLexicon.containsKey(word))
-        continue;
-      WordConnotation wc = connotationLexicon.get(word);
-      features.addWord(wc);
-      
-      //System.out.println(word);
-      //System.out.println("-- pos: " + wc.isPositive() + " | neg: " + wc.isNegative() + " | strong: " + wc.isStrong() + " | weak: " + wc.isWeak());
-      //System.out.println();
-    }
-    
-    in.close();
-    
-    return features;
-    
-  }
-  
-
-  private static void loadLexicon() throws FileNotFoundException {
-
-    CSVParser parser =
-        new CSVParserBuilder()
-        .withSeparator(';')
-        .build();
-
-    CSVReader reader =
-        new CSVReaderBuilder(new FileReader(lexiconInputFile))
-        .withCSVParser(parser)
-        .build();
-
-    Iterator<String[]> it = reader.iterator();    
-    String[] csvHeader = it.next();
-
-    Map<String, Integer> csvColMap = getColIndices(csvHeader);    
-
-    while (it.hasNext()) {
-      String[] wordInfo = it.next();
-      String word = wordInfo[csvColMap.get("Entry")];
-      word = word
-          .replaceAll(wordDelimiterPattern.pattern(), "")
-          .toLowerCase();
-      if (connotationLexicon.containsKey(word))
-        continue;
-      //System.out.println("Test: word is " + word);
-      //System.out.println("Positiv: " + wordInfo[csvColMap.get("Positiv")]);
-      boolean positive = wordInfo[csvColMap.get("Positiv")].equals("Positiv");
-      boolean negative = wordInfo[csvColMap.get("Negativ")].equals("Negativ");
-      boolean strong = wordInfo[csvColMap.get("Strong")].equals("Strong");
-      boolean weak = wordInfo[csvColMap.get("Weak")].equals("Weak");;
-      WordConnotation wc = new WordConnotation(word, positive, negative, strong, weak);
-      connotationLexicon.put(word, wc);
-      //System.out.println("is positive: " + wc.isPositive());
-    }
-
-
-  }
-
-  private static Map<String, Integer> getColIndices (String[] csvHeader) {
-    // Map to map column indices in the csv and the relevant attributes
-    Map<String, Integer> csvColMap = new HashMap<>();
-
-    for (int i = 0; i < csvHeader.length; i++) {
-      if (relevantCols.contains(csvHeader[i])) {
-        csvColMap.put(csvHeader[i], i);
-      }
-    }
-
-    return csvColMap;    
-  }
-}
-=======
 package ch.bfh.bti7535.w2017.midori.movieclassifier.converter;
 
 import java.io.BufferedWriter;
@@ -206,8 +45,7 @@ public class ArffGenerator {
   private final static File csvOutputFile = new File("data" + File.separator + "movie-reviews.csv");
   private final static File arffOutputFile = new File("data" + File.separator + "movie-reviews.arff");
 
-  //private final static List<String> relevantCols = Arrays.asList("Entry", "Positiv", "Negativ", "Hostile", "Strong", "Power", "Weak", "Active", "Passive", "Pleasur");
-  private final static List<String> relevantCols = Arrays.asList("Entry", "Positiv", "Negativ", "Hostile", "Strong", "Power", "Weak", "Pleasur");
+  private final static List<String> relevantCols = Arrays.asList("Entry", "Positiv", "Negativ", "Hostile", "Strong", "Power", "Weak");
   private final static Pattern wordRecognitionPattern = Pattern.compile(".*[A-Za-z]+.*");
   private final static Pattern punctuationRecognitionPattern = Pattern.compile("[.,!?\\-]");
   private final static Pattern wordDelimiterPattern = Pattern.compile("[^A-Za-z]"); 
@@ -249,8 +87,7 @@ public class ArffGenerator {
     BufferedWriter writer = new BufferedWriter(new FileWriter(arffOutputFile));
     writer.write(data.toString());
     writer.flush();
-    writer.close();
-    
+    writer.close();   
   }
   
   private static String loadCommentFile(File file) throws FileNotFoundException {
@@ -259,7 +96,6 @@ public class ArffGenerator {
     String fileContent = scanner.useDelimiter("\\Z").next();
     scanner.close();
     return fileContent;
-    
   }
   
   private static String addNegativeLabels(String comment) {
@@ -282,15 +118,13 @@ public class ArffGenerator {
       parts[i] = String.join(" ", words);
       
       //System.out.println("--- orig: " + part);
-      //System.out.println("--- conv: " + parts[i]);
-      
+      //System.out.println("--- conv: " + parts[i]);    
     }
     
     String convertedText = String.join(" ", parts);
     //System.out.println(convertedText);
     
-    return convertedText;
-    
+    return convertedText;   
   }
   
   private static InstanceFeatures extractFeatures(String text, Label label) {
@@ -313,13 +147,11 @@ public class ArffGenerator {
       WordConnotation wc = connotationLexicon.get(word);
       features.addWord(wc, negated);
     }
-    
-      
+         
       //System.out.println(word);
       //System.out.println("-- pos: " + wc.isPositive() + " | neg: " + wc.isNegative() + " | strong: " + wc.isStrong() + " | weak: " + wc.isWeak());
       //System.out.println();
-    
-        
+      
     return features;
     
   }
@@ -356,16 +188,10 @@ public class ArffGenerator {
       boolean negative = wordInfo[csvColMap.get("Negativ")].equals("Negativ");
       boolean strong = wordInfo[csvColMap.get("Strong")].equals("Strong");
       boolean weak = wordInfo[csvColMap.get("Weak")].equals("Weak");
-      //boolean active = wordInfo[csvColMap.get("Active")].equals("Active");
-      //boolean passive = wordInfo[csvColMap.get("Passive")].equals("Passive");
-      boolean pleasur = wordInfo[csvColMap.get("Pleasur")].equals("Pleasur");
-//      WordConnotation wc = new WordConnotation(word, positive, negative, strong, weak, active, passive, pleasur);
-      WordConnotation wc = new WordConnotation(word, positive, negative, strong, weak, pleasur);
+      WordConnotation wc = new WordConnotation(word, positive, negative, strong, weak);
       connotationLexicon.put(word, wc);
       //System.out.println("is positive: " + wc.isPositive());
     }
-
-
   }
 
   private static Map<String, Integer> getColIndices (String[] csvHeader) {
@@ -381,4 +207,3 @@ public class ArffGenerator {
     return csvColMap;    
   }
 }
->>>>>>> branch 'master' of https://github.com/stoeffu/ch.bfh.bti7535.w2017.midori
--- src/ch/bfh/bti7535/w2017/midori/movieclassifier/converter/InstanceFeatures.java
+++ src/ch/bfh/bti7535/w2017/midori/movieclassifier/converter/InstanceFeatures.java
@@ -1,75 +1,3 @@
-<<<<<<< HEAD
-package ch.bfh.bti7535.w2017.midori.movieclassifier.converter;
-
-import ch.bfh.bti7535.w2017.midori.movieclassifier.converter.ArffGenerator.Label;
-
-public class InstanceFeatures {
-
-  private Label label = null;
-  private int positive, negative, strong, weak = 0;
-  
-  public static String[] titles() {
-    String[] str = {
-    		"positive", "negative", "strong", "weak", "pleasur", "class"
-    };
-    return str;
-  }
-  
-  public InstanceFeatures(Label label) {
-    this.label = label;
-  }
-  
-  public int getPositive() {
-    return positive;
-  }
-
-  public int getNegative() {
-    return negative;
-  }
-
-  public int getStrong() {
-    return strong;
-  }
-
-  public int getWeak() {
-    return weak;
-  }
-  
-  public String[] toStringArray() {
-    String[] str = {
-        String.valueOf(positive), String.valueOf(negative), String.valueOf(strong), String.valueOf(weak), label.name()
-    };    
-    return str;
-  }
-  
-  public void addWord(WordConnotation wc) {
-    if(wc.isPositive())
-      addPositive();
-    if(wc.isNegative())
-      addNegative();
-    if(wc.isStrong())
-      addStrong();
-    if(wc.isWeak())
-      addWeak();
-  }
-  
-  private void addPositive() {
-    positive++;
-  }
-  
-  private void addNegative() {
-    negative++;
-  }
-  
-  private void addStrong() {
-    strong++;
-  }
-  
-  private void addWeak() {
-    weak++;
-  }
-}
-=======
 package ch.bfh.bti7535.w2017.midori.movieclassifier.converter;
 
 import ch.bfh.bti7535.w2017.midori.movieclassifier.converter.ArffGenerator.Label;
@@ -91,13 +19,11 @@ public class InstanceFeatures {
   }
   
   private Label label = null;
-  //private int positive, negative, strong, weak, active, passive, pleasur = 0;
-  private double positive, negative, pleasur = 0;
+  private double positive, negative = 0;
   
   public static String[] titles() {
     String[] str = {
-        //"positive", "negative", "strong", "weak", "active", "passive", "pleasur", "class"
-    		"positive", "negative", "pleasur", "class"
+    		"positive", "negative", "class"
     };
     return str;
   }
@@ -114,23 +40,9 @@ public class InstanceFeatures {
     return negative;
   }
 
-  
-  /*public int getActive() {
-	  return active;
-  }
-  
-  public int getPassive() {
-	  return passive;
-  }*/
-  
-  public double getPleasur() {
-	  return pleasur;
-  }
-  
   public String[] toStringArray() {
     String[] str = {
-        //String.valueOf(positive), String.valueOf(negative), String.valueOf(strong), String.valueOf(weak),String.valueOf(active), String.valueOf(passive), String.valueOf(pleasur), label.name()
-        String.valueOf(positive), String.valueOf(negative), String.valueOf(pleasur), label.name()
+        String.valueOf(positive), String.valueOf(negative), label.name()
     };    
     return str;
   }
@@ -154,12 +66,6 @@ public class InstanceFeatures {
       else
         addNegative(weight);
     }      
-    /*if(wc.isActive())
-      addActive();
-    if(wc.isPassive())
-    	addPassive();*/
-    if(wc.isPleasur())
-    	addPleasur();
   }
   
   private void addPositive(Weight weight) {
@@ -169,25 +75,4 @@ public class InstanceFeatures {
   private void addNegative(Weight weight) {
     negative = negative + weight.getValue();
   }
-  
-  /*private void addStrong() {
-    strong++;
-  }
-  
-  private void addWeak() {
-    weak++;
-  }
-  
-  private void addActive() {
-	  active++;
-  }
-  
-  private void addPassive() {
-	  passive++;
-  }*/
-  private void addPleasur() {
-	  pleasur++;
-  }
-  
 }
->>>>>>> branch 'master' of https://github.com/stoeffu/ch.bfh.bti7535.w2017.midori
