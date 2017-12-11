060f294ee3675f82e1007020e17a14aa731d5ccd
Steven Cardini <steven@cardini.ch>
2017-12-11 18:39:16 +0100

    beautifies app.java

--- src/ch/bfh/bti7535/w2017/midori/movieclassifier/App.java
+++ src/ch/bfh/bti7535/w2017/midori/movieclassifier/App.java
@@ -1,5 +1,6 @@
 package ch.bfh.bti7535.w2017.midori.movieclassifier;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -14,16 +15,14 @@ import weka.classifiers.bayes.NaiveBayes;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args ) throws Exception
-    {
-        // https://weka.wikispaces.com/Programmatic+Use
-        //InstanceLoader loader = new InstanceLoader();
-    	
-    	ArffImporter imp = new ArffImporter();
-    	//imp.importArff("data\\movie-reviews.arff");
-    	
-    	Stratifier.Fold(10, "data\\movie-reviews.arff");
-    }
+public class App {
+  public static void main(String[] args) throws Exception {
+    // https://weka.wikispaces.com/Programmatic+Use
+    // InstanceLoader loader = new InstanceLoader();
+
+    // ArffImporter imp = new ArffImporter();
+    // imp.importArff("data\\movie-reviews.arff");
+
+    Stratifier.Fold(10, "data" + File.separator + "movie-reviews.arff");
+  }
 }
