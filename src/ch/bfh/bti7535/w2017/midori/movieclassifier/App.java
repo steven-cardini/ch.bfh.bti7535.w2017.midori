4df651cf3ffd94f28adfb5dac804816aeccd174f
Steven Cardini <steven@cardini.ch>
2018-01-14 16:54:35 +0100

    improves code of App.java

--- src/ch/bfh/bti7535/w2017/midori/movieclassifier/App.java
+++ src/ch/bfh/bti7535/w2017/midori/movieclassifier/App.java
@@ -3,11 +3,16 @@ package ch.bfh.bti7535.w2017.midori.movieclassifier;
 import java.io.File;
 import java.util.Random;
 
+import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
 import weka.classifiers.bayes.NaiveBayesMultinomial;
+import weka.classifiers.rules.ZeroR;
 import weka.core.Instances;
 
 
+/**
+ * Imports ARFF file with instances and builds baseline as well as naive Bayes classifiers to evaluate the accuracy
+ */
 public class App {
 
   private final static File arffInputFile = new File("data" + File.separator + "movie-reviews.arff");
@@ -15,29 +20,38 @@ public class App {
 
   public static void main(String[] args) throws Exception {
 
-    naiveBayes();
+    Instances data = ArffImporter.importArff(arffInputFile);
+
+    Classifier baselineClassifier = new ZeroR();
+    Classifier naiveBayesClassifier = new NaiveBayesMultinomial();
+
+    System.out.println("--- BASELINE ---");
+    tenfoldCrossValidation(data, baselineClassifier);
+    System.out.println("--- NAIVE BAYES ---");
+    tenfoldCrossValidation(data, naiveBayesClassifier);
 
   }
 
-  private static void naiveBayes() throws Exception {
+  private static void tenfoldCrossValidation(Instances data, Classifier classifier) throws Exception {
+
+    // randomize and stratify the data
     Random rand = new Random();
+    Instances randData = new Instances(data);
+    randData.randomize(rand);
+    randData.stratify(numFolds);
 
-    NaiveBayesMultinomial cl = new NaiveBayesMultinomial();
-    Instances inst = ArffImporter.importArff(arffInputFile);
-
-    inst.randomize(rand);
-    inst.stratify(numFolds);
-
-    Evaluation eval = new Evaluation(inst);
+    Evaluation eval = new Evaluation(randData);
 
     for (int n = 0; n < numFolds; n++) {
-      Instances train = inst.trainCV(numFolds, n);
-      Instances test = inst.testCV(numFolds, n);
 
-      cl.buildClassifier(train);
-      eval.evaluateModel(cl, test);
-      System.out.println(eval.pctCorrect());
+      Instances train = randData.trainCV(numFolds, n);
+      Instances test = randData.testCV(numFolds, n);
+
+      classifier.buildClassifier(train);
+      eval.evaluateModel(classifier, test);
+      System.out.println("Accuracy (" + n + "): " + eval.pctCorrect());
     }
 
   }
+
 }
